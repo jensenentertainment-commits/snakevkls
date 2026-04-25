95c8d517d245d7fc0cd10cc5a3c027addde83740
Lars R. Jensen <jensen.entertainment@gmail.com>
2026-04-25 21:31:15 +0200

    Add basic auth protection

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,28 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+
+const USERNAME = "snake";
+const PASSWORD = "snake123";
 
 export function middleware(request: NextRequest) {
-  const auth = request.headers.get("authorization");
+  const authHeader = request.headers.get("authorization");
 
-  if (auth !== "Bearer snake123") {
-    return new Response("Unauthorized", {
-      status: 401,
-      headers: {
-        "WWW-Authenticate": 'Basic realm="Secure Area"',
-      },
-    });
+  if (authHeader) {
+    const basicAuth = authHeader.split(" ")[1];
+    const [user, password] = atob(basicAuth).split(":");
+
+    if (user === USERNAME && password === PASSWORD) {
+      return NextResponse.next();
+    }
   }
 
-  return NextResponse.next();
+  return new Response("Authentication required", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="SNAKE VKLS"',
+    },
+  });
 }
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|robots.txt).*)"],
+};
